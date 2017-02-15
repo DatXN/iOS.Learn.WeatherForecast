bb826351b13b25e7367d820505e6adc9a542e1e7
dat.nguyen <datnguyen231@gmail.com>
2017-02-15 17:32:56 +0700

    Init

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Nguyễn Xuân Đạt on 2/15/17.
+//  Copyright © 2017 Nguyễn Xuân Đạt. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
